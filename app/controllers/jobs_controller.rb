3d1847fd181829b032a08fa8beadfcbc7a2c9ea8
kuensan <king.the.strength@gmail.com>
2019-08-16 00:21:49 +0900

    基本的なCRUDの処理追加

--- app/controllers/jobs_controller.rb
+++ app/controllers/jobs_controller.rb
@@ -12,12 +12,25 @@ class JobsController < ApplicationController
   end
 
   def edit
+    @job = Job.find(params[:id])
   end
 
   def create
     job = Job.new(job_params)
     job.save!
     redirect_to jobs_url, notice: "タスク「#{job.name}」を登録しました。"
+  end
+
+  def update
+    job = Job.find(params[:id])
+    job.update!(job_params)
+    redirect_to jobs_url, notice: "タスク「#{job.name}」を更新しました。"
+  end
+
+  def destroy
+    job = Job.find(params[:id])
+    job.destroy
+    redirect_to jobs_url, notice: "タスク「#{job.name}」を削除しました。"
   end
 
   private
